f7da3d97962046429e243be732273f7d47558447
Alex <goodmorozov@icloud.com>
2023-10-21 19:22:08 +0300

    feat(shared): create ui form

---
+++ src/shared/ui/form/ui.tsx
@@ -0,0 +1,47 @@
+import style from './style.module.scss'
+import { ReactNode, createElement } from 'react'
+import { DefaultValues, UseFormResetField, useForm } from 'react-hook-form'
+
+export interface IOnSubmit<T> {
+  data: T
+  resetField: UseFormResetField<any>
+}
+export interface IForm<T> {
+  defaultValues: DefaultValues<T> | undefined
+  onSubmit: ({ data, resetField }: IOnSubmit<T>) => void
+  children: ReactNode
+}
+
+export const Form = <T extends object>({
+  defaultValues,
+  onSubmit,
+  children,
+}: IForm<T>) => {
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { errors },
+  } = useForm({ defaultValues, mode: 'onChange' })
+  return (
+    <form
+      className={style.form}
+      onSubmit={handleSubmit((data) => {
+        onSubmit({ data, resetField })
+      })}
+    >
+      {Array.isArray(children)
+        ? children.map((child) =>
+            child.props?.name
+              ? createElement(child.type, {
+                  ...child.props,
+                  register,
+                  errors,
+                  key: child.props.name,
+                })
+              : child
+          )
+        : children}
+    </form>
+  )
+}
