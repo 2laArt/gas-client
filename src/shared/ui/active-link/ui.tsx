7f5224832c19f103212406013a8cc20933fe8c83
Alex <goodmorozov@icloud.com>
2023-11-01 21:54:25 +0300

    feat(shared): create ui active-link

---
+++ src/shared/ui/active-link/ui.tsx
@@ -0,0 +1,26 @@
+import style from './style.module.scss'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { FC, PropsWithChildren } from 'react'
+
+interface IActiveLink extends PropsWithChildren {
+  href: string
+  asPath: string
+  className?: string
+}
+export const ActiveLink: FC<IActiveLink> = ({
+  href,
+  asPath,
+  children,
+  className,
+}) => {
+  const isActive = asPath.includes(href)
+  return (
+    <Link
+      href={'/' + href}
+      className={clsx(style.link, isActive && style.link_active, className)}
+    >
+      {children}
+    </Link>
+  )
+}
