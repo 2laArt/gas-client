71c8b9773c341e7dab2aed4a314562e2f0479396
Alex <goodmorozov@icloud.com>
2023-11-05 18:39:48 +0300

    feat(custom): sprite: social, payment | (shared): icon create social, payment

--- src/shared/ui/icon/sprite.ts
+++ src/shared/ui/icon/sprite.ts
@@ -8,4 +8,7 @@ export interface Sprite {
     | 'cart'
     | 'added-cart'
     | 'pen'
+
+  social: 'vk' | 'facebook' | 'youtube' | 'instagram'
+  payment: 'apple-pay' | 'google-pay' | 'master-card' | 'visa'
 }
