f16fcde5d3505697dc0bdea89f7aa8435a961d09
Alex <goodmorozov@icloud.com>
2023-12-20 17:03:40 +0300

    feat(pages): (product): added mobile slider

--- src/pages/product/ui/images-section/ui.tsx
+++ src/pages/product/ui/images-section/ui.tsx
@@ -3,67 +3,72 @@ import Image from 'next/image'
 import { useState, type FC } from 'react'
 import { useMediaQuery } from 'shared/lib'
 import { Slider } from 'shared/ui'
+import { EffectCards } from 'swiper/modules'
+import { type SwiperOptions } from 'swiper/types'
 
 export const ProductPageImages: FC<{ images: string[] }> = ({ images }) => {
   const [activeImg, setActiveImg] = useState<string>(images[0])
   const is768 = useMediaQuery(768)
-
+  const options: SwiperOptions = {
+    spaceBetween: 12,
+    scrollbar: {
+      dragClass: style.scrollbar,
+    },
+    autoplay: false,
+    loop: false,
+  }
+  const optionsMobile: SwiperOptions = {
+    effect: 'cards',
+    autoplay: false,
+    loop: true,
+    centeredSlides: true,
+  }
   const Item = (item: string, idx: number) => (
     <div className={style.image} key={idx}>
       <Image
         src={item}
         width={648}
         height={488}
-        alt={`image ${idx}`}
+        alt={`boiler image ${idx}`}
         priority
-        onClick={() => is768 && setActiveImg(item)}
+        onClick={() => !is768 && setActiveImg(item)}
       />
     </div>
   )
   return (
     <div>
-      {!is768 && (
-        <div className={style.image_active}>
-          <Image
-            src={activeImg}
-            alt="product"
-            width={648}
-            height={488}
-            priority
+      {!is768 ? (
+        <>
+          <div className={style.image_active}>
+            <Image
+              src={activeImg}
+              alt="product"
+              width={648}
+              height={488}
+              priority
+            />
+          </div>
+          <div className={style.images}>
+            <Slider
+              navigation={false}
+              items={images}
+              renderItem={Item}
+              options={options}
+            />
+          </div>
+        </>
+      ) : (
+        <div className={style.images}>
+          <Slider
+            navigation={false}
+            slideClassName={style.slide_mobile}
+            items={images}
+            renderItem={Item}
+            options={optionsMobile}
+            modules={[EffectCards]}
           />
         </div>
       )}
-      <div className={style.images}>
-        <Slider
-          navigation={false}
-          slideClassName={style.slide}
-          slideActiveClass={style.active}
-          items={images}
-          renderItem={Item}
-          options={{
-            breakpoints: {
-              768: {
-                slidesPerView: 'auto',
-                centeredSlides: false,
-                loop: false,
-                slideActiveClass: undefined,
-                spaceBetween: 12,
-              },
-              518: {
-                slidesPerView: 1.5,
-              },
-            },
-            slidesPerView: 1.2,
-            centeredSlides: true,
-            scrollbar: {
-              dragClass: style.scrollbar,
-            },
-            autoplay: false,
-            loop: true,
-            spaceBetween: 20,
-          }}
-        />
-      </div>
     </div>
   )
 }
